e2233567a2076043d94ed7653ce3978865940eb9
suap-dev <suap.dev@gmail.com>
2023-01-16 14:47:27 +0100

    "controllable" -> "player"

--- game/components/player.lua
+++ game/components/player.lua
@@ -1,6 +1,6 @@
 local Component = require("modules.concord.component")
 
-return Component("controlable", function(c, key_up, key_down, base_speed)
+return Component("player", function(c, key_up, key_down, base_speed)
     c.up = key_up or "up"
     c.down = key_down or "down"
     c.base_speed = base_speed or 0
--- game/systems/player_control.lua
+++ game/systems/player_control.lua
@@ -1,14 +1,14 @@
 local System = require("modules.concord.system")
 
 local system = System({
-    pool = { "position", "controlable" }
+    pool = { "position", "player" }
 })
 function system:update(dt)
     for _, e in ipairs(self.pool) do
-        if love.keyboard.isDown(e.controlable.up) and not (e.collides and e.collides.at.top) then
-            e:give("velocity", 0, -e.controlable.base_speed)
-        elseif love.keyboard.isDown(e.controlable.down) and not (e.collides and e.collides.at.bot) then
-            e:give("velocity", 0, e.controlable.base_speed)
+        if love.keyboard.isDown(e.player.up) and not (e.collides and e.collides.at.top) then
+            e:give("velocity", 0, -e.player.base_speed)
+        elseif love.keyboard.isDown(e.player.down) and not (e.collides and e.collides.at.bot) then
+            e:give("velocity", 0, e.player.base_speed)
         else
             e:remove("velocity")
         end
