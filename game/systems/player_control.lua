3448183e10067473a379eab0786e05e3010083d2
suap-dev <suap.dev@gmail.com>
2023-01-16 10:45:19 +0100

    handle rocket movement by adding/removing velocity component

--- game/systems/player_control.lua
+++ game/systems/player_control.lua
@@ -1,16 +1,16 @@
 local System = require("modules.concord.system")
 
 local player_control = System({
-    pool = { "position", "controlable", "velocity" }
+    pool = { "position", "controlable"}
 })
 function player_control:update(dt)
     for _, e in ipairs(self.pool) do
         if love.keyboard.isDown(e.controlable.up) then
-            e.velocity.y = -e.controlable.base_speed
+            e:give("velocity", 0, -e.controlable.base_speed)
         elseif love.keyboard.isDown(e.controlable.down) then
-            e.velocity.y = e.controlable.base_speed
+            e:give("velocity", 0, e.controlable.base_speed)
         else
-            e.velocity.y = 0
+            e:remove("velocity")
         end
     end
 end
